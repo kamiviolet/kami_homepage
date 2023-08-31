161f080ed1e56bec3fe9ff6b96989e1da1e271e3
Kami Lam <kamiviolet@gmail.com>
2023-08-31 21:16:44 +0100

    gallery and subsection

---
+++ src/lib/constants/svg_collection.tsx
@@ -0,0 +1,218 @@
+import { GallerySubsectionType } from "@/types/gallery_subsection.type";
+
+const svgList: GallerySubsectionType = {
+  name: "Interface Elements",
+  description: "The elements which can be used to design interface.",
+  list: [
+    {
+      name: "blind_dw_hr_original",
+      file: "/svg_works/interface_elements/blind_dw_hr_original.svg",
+    },
+    {
+      name: "blind_dw_hr_taped",
+      file: "/svg_works/interface_elements/blind_dw_hr_taped.svg",
+    },
+    {
+      name: "blind_dw_vt_angle",
+      file: "/svg_works/interface_elements/blind_dw_vt_angle.svg",
+    },
+    {
+      name: "blind_dw_vt_color",
+      file: "/svg_works/interface_elements/blind_dw_vt_color.svg",
+    },
+    {
+      name: "blind_dw_vt_flat",
+      file: "/svg_works/interface_elements/blind_dw_vt_flat.svg",
+    },
+    {
+      name: "blind_open_hr_original",
+      file: "/svg_works/interface_elements/blind_open_hr_original.svg",
+    },
+    {
+      name: "blind_open_hr_taped",
+      file: "/svg_works/interface_elements/blind_open_hr_taped.svg",
+    },
+    {
+      name: "blind_open_vt_angle",
+      file: "/svg_works/interface_elements/blind_open_vt_angle.svg",
+    },
+    {
+      name: "blind_open_vt_color",
+      file: "/svg_works/interface_elements/blind_open_vt_color.svg",
+    },
+    {
+      name: "blind_open_vt_flat",
+      file: "/svg_works/interface_elements/blind_open_vt_flat.svg",
+    },
+    {
+      name: "cctv_style_1",
+      file: "/svg_works/interface_elements/cctv_style_1.svg",
+    },
+    {
+      name: "cctv_style_2",
+      file: "/svg_works/interface_elements/cctv_style_2.svg",
+    },
+    {
+      name: "cctv_style_3",
+      file: "/svg_works/interface_elements/cctv_style_3.svg",
+    },
+    {
+      name: "fan_off_original",
+      file: "/svg_works/interface_elements/fan_off_original.svg",
+    },
+    {
+      name: "fan_off_stand_color",
+      file: "/svg_works/interface_elements/fan_off_stand_color.svg",
+    },
+    {
+      name: "fan_off_stand",
+      file: "/svg_works/interface_elements/fan_off_stand.svg",
+    },
+    {
+      name: "fan_on_original",
+      file: "/svg_works/interface_elements/fan_on_original.svg",
+    },
+    {
+      name: "fan_on_stand_color",
+      file: "/svg_works/interface_elements/fan_on_stand_color.svg",
+    },
+    {
+      name: "fan_on_stand",
+      file: "/svg_works/interface_elements/fan_on_stand.svg",
+    },
+    {
+      name: "intercom_style_1",
+      file: "/svg_works/interface_elements/intercom_style_1.svg",
+    },
+    {
+      name: "intercom_style_2",
+      file: "/svg_works/interface_elements/intercom_style_2.svg",
+    },
+    {
+      name: "light_off_bulb",
+      file: "/svg_works/interface_elements/light_off_bulb.svg",
+    },
+    {
+      name: "light_off_ceiling_lamp_rd",
+      file: "/svg_works/interface_elements/light_off_ceiling_lamp_rd.svg",
+    },
+    {
+      name: "light_off_ceiling_lamp",
+      file: "/svg_works/interface_elements/light_off_ceiling_lamp.svg",
+    },
+    {
+      name: "light_off_desklamp",
+      file: "/svg_works/interface_elements/light_off_desklamp.svg",
+    },
+    {
+      name: "light_on_bulb",
+      file: "/svg_works/interface_elements/light_on_bulb.svg",
+    },
+    {
+      name: "light_on_ceiling_lamp_rd",
+      file: "/svg_works/interface_elements/light_on_ceiling_lamp_rd.svg",
+    },
+    {
+      name: "light_on_ceiling_lamp",
+      file: "/svg_works/interface_elements/light_on_ceiling_lamp.svg",
+    },
+    {
+      name: "light_on_desklamp",
+      file: "/svg_works/interface_elements/light_on_desklamp.svg",
+    },
+    {
+      name: "lock_style_1",
+      file: "/svg_works/interface_elements/lock_style_1.svg",
+    },
+    {
+      name: "lock_style_2",
+      file: "/svg_works/interface_elements/lock_style_2.svg",
+    },
+    {
+      name: "lock_style_3",
+      file: "/svg_works/interface_elements/lock_style_3.svg",
+    },
+    {
+      name: "setting_single_1",
+      file: "/svg_works/interface_elements/setting_single_1.svg",
+    },
+    {
+      name: "setting_single_2",
+      file: "/svg_works/interface_elements/setting_single_2.svg",
+    },
+    {
+      name: "setting_twin_1",
+      file: "/svg_works/interface_elements/setting_twin_1.svg",
+    },
+    {
+      name: "setting_twin_2",
+      file: "/svg_works/interface_elements/setting_twin_2.svg",
+    },
+    {
+      name: "sound_off_angle",
+      file: "/svg_works/interface_elements/sound_off_angle.svg",
+    },
+    {
+      name: "sound_off_hifi_color",
+      file: "/svg_works/interface_elements/sound_off_hifi_color.svg",
+    },
+    {
+      name: "sound_off_hifi",
+      file: "/svg_works/interface_elements/sound_off_hifi.svg",
+    },
+    {
+      name: "sound_off_rd",
+      file: "/svg_works/interface_elements/sound_off_rd.svg",
+    },
+    {
+      name: "sound_on_angle_note",
+      file: "/svg_works/interface_elements/sound_on_angle_note.svg",
+    },
+    {
+      name: "sound_on_angle_radiate",
+      file: "/svg_works/interface_elements/sound_on_angle_radiate.svg",
+    },
+    {
+      name: "sound_on_hifi_color",
+      file: "/svg_works/interface_elements/sound_on_hifi_color.svg",
+    },
+    {
+      name: "sound_on_hifi",
+      file: "/svg_works/interface_elements/sound_on_hifi.svg",
+    },
+    {
+      name: "sound_on_rd_note",
+      file: "/svg_works/interface_elements/sound_on_rd_note.svg",
+    },
+    {
+      name: "stat_bars_colors",
+      file: "/svg_works/interface_elements/stat_bars_colors.svg",
+    },
+    {
+      name: "stat_bars",
+      file: "/svg_works/interface_elements/stat_bars.svg",
+    },
+    {
+      name: "stat_line",
+      file: "/svg_works/interface_elements/stat_line.svg",
+    },
+    {
+      name: "temp_style_1_color",
+      file: "/svg_works/interface_elements/temp_style_1_color.svg",
+    },
+    {
+      name: "temp_style_1",
+      file: "/svg_works/interface_elements/temp_style_1.svg",
+    },
+    {
+      name: "temp_style_2_color",
+      file: "/svg_works/interface_elements/temp_style_2_color.svg",
+    },
+    {
+      name: "temp_style_2",
+      file: "/svg_works/interface_elements/temp_style_2.svg",
+    },
+  ],
+};
+
+export default svgList;
---
+++ src/types/gallery_subsection.type.ts
@@ -0,0 +1,10 @@
+export type GallerySubsectionType = {
+  name: string;
+  description: string;
+  list: any[];
+};
+
+export type ThumbnailType = {
+  name: string;
+  file: string;
+};
