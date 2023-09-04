34de4f78e3b3ba69281ccaafbfee0e5980378008
Kami Lam <kamiviolet@gmail.com>
2023-09-04 12:59:24 +0100

    update footer

--- src/app/_footer.tsx
+++ src/app/_footer.tsx
@@ -4,6 +4,7 @@ export default function Footer(): React.ReactNode {
   return (
     <footer className="w-2/3 border-t-4 border-information max-w-[1280px] text-center py-8">
       <p className="text-sm">By Kami Lam, 2023</p>
+      <p className="text-sm">Built with Next & MongoDB</p>
     </footer>
   );
 }
